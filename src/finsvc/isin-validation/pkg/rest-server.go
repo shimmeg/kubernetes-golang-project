7a707cb92763d17e53778c79fc3abbd2d48a4430
Anton Efimow <anton.efimov@360t.com>
2021-04-21 22:20:43 +0200

    na: additional endpoint for health check [Anton Efimow] [V]

--- src/finsvc/isin-validation/pkg/rest-server.go
+++ src/finsvc/isin-validation/pkg/rest-server.go
@@ -3,35 +3,43 @@ package pkg
 import (
 	"encoding/json"
 	"finsvc/isin-validation/pkg/validation"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
+const ResponseOk = "{\"staus\": \"OK\"}"
+
 func StartServer(addr string, endpoints map[string]func(writer http.ResponseWriter, request *http.Request)) {
 	isinMux := http.NewServeMux()
 
-	fmt.Println("Will create endpoints: ", endpoints)
+	log.Println("Will create endpoints: ", endpoints)
 	for endpoint, handler := range endpoints {
 		isinMux.HandleFunc(endpoint, handler)
 	}
 
 	restHandler := accessLogMiddleware(isinMux)
+	restHandler = contentTypeMiddleware(restHandler)
 
-	fmt.Println("Starting server at the address: ", addr)
+	log.Println("Starting server at the address: ", addr)
 	err := http.ListenAndServe(addr, restHandler)
 	if err != nil {
 		log.Fatal("Error happened during starting server ", err)
 	}
 }
 
+func contentTypeMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+	})
+}
+
 func accessLogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("accessLog", r.URL.Path)
+		log.Println("accessLog", r.URL.Path)
 		start := time.Now()
 		next.ServeHTTP(w, r)
-		fmt.Printf("[%s] %s, %s %s\n", r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
+		log.Printf("[%s] %s, %s %s\n", r.Method, r.RemoteAddr, r.URL.Path, time.Since(start))
 	})
 }
 
@@ -49,6 +57,16 @@ func IsinEndpointGenerator(validate func(isin string) *validation.Result) func(w
 		_, err = w.Write(marshalledResult)
 		if err != nil {
 			log.Println("Error during sending the result", marshalledResult, err)
+			return
+		}
+	}
+}
+
+func HealthCheckEndpointGenerator() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(ResponseOk))
+		if err != nil {
+			log.Println("Error during sending the result", err)
 			return
 		}
 	}
