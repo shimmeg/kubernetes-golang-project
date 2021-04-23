ae35da4aca7c9dc1190f990385e4bb2d646d3721
Anton Efimow <anton.efimov@360t.com>
2021-04-23 14:03:33 +0200

    na: - fixed few minor issues. changed handler from '/health/' to '/health' - new version of docker container [Anton Efimow] [V][Anton Efimow] [V]

--- src/finsvc/isin-validation/cmd/main.go
+++ src/finsvc/isin-validation/cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	endpoints := make(map[string]func(writer http.ResponseWriter, request *http.Request))
 	endpoints["/isin/"] = pkg.IsinEndpointGenerator(validation.ValidateIsin)
-	endpoints["/health/"] = pkg.HealthCheckEndpointGenerator()
+	endpoints["/health"] = pkg.HealthCheckEndpointGenerator()
 
 	pkg.StartServer(":8000", endpoints)
 }
